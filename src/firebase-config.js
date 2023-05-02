aab53a4ba2ff8f1623cdbe5ab9b9c0e9959ed0bb
Jorge Escoto <jdanielet@outlook.com>
2023-05-02 09:39:34 -0600

    push

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -2,7 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "@firebase/firestore";
 
-const firebaseConfig = {};
+const firebaseConfig = {
+  apiKey: "AIzaSyAZ5Ds3Fm9qCN-kPsMjlktv8lzRja2hvls",
+  authDomain: "viajes-3beda.firebaseapp.com",
+  projectId: "viajes-3beda",
+  storageBucket: "viajes-3beda.appspot.com",
+  messagingSenderId: "1088276695278",
+  appId: "1:1088276695278:web:c5a1129260b496c3d50bff",
+};
 
 const app = initializeApp(firebaseConfig);
 
