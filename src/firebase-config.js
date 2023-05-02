62d47e08ed370eda6df6bbeef5351f08fb7d6ba5
Jorge Escoto <jdanielet@outlook.com>
2023-05-02 09:40:32 -0600

    removido link de db

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -2,14 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "@firebase/firestore";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyAZ5Ds3Fm9qCN-kPsMjlktv8lzRja2hvls",
-  authDomain: "viajes-3beda.firebaseapp.com",
-  projectId: "viajes-3beda",
-  storageBucket: "viajes-3beda.appspot.com",
-  messagingSenderId: "1088276695278",
-  appId: "1:1088276695278:web:c5a1129260b496c3d50bff",
-};
+const firebaseConfig = {};
 
 const app = initializeApp(firebaseConfig);
 
